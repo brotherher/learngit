70023cba230074c600c5607e2f96ed58a299936c
brotherher <32901652+brotherher@users.noreply.github.com>
2018-08-14 09:51:52 +0800

    Update markdown2.py

--- py file/markdown2.py
+++ py file/markdown2.py
@@ -40,52 +40,12 @@ text-to-HTML conversion tool for web writers.
 Supported extra syntax options (see -x|--extras option below and
 see <https://github.com/trentm/python-markdown2/wiki/Extras> for details):
 
-* code-friendly: Disable _ and __ for em and strong.
-* cuddled-lists: Allow lists to be cuddled to the preceding paragraph.
-* fenced-code-blocks: Allows a code block to not have to be indented
-  by fencing it with '```' on a line before and after. Based on
-  <http://github.github.com/github-flavored-markdown/> with support for
-  syntax highlighting.
-* footnotes: Support footnotes as in use on daringfireball.net and
-  implemented in other Markdown processors (tho not in Markdown.pl v1.0.1).
-* header-ids: Adds "id" attributes to headers. The id value is a slug of
-  the header text.
-* html-classes: Takes a dict mapping html tag names (lowercase) to a
-  string to use for a "class" tag attribute. Currently only supports
-  "pre" and "code" tags. Add an issue if you require this for other tags.
-* markdown-in-html: Allow the use of `markdown="1"` in a block HTML tag to
-  have markdown processing be done on its contents. Similar to
-  <http://michelf.com/projects/php-markdown/extra/#markdown-attr> but with
-  some limitations.
-* metadata: Extract metadata from a leading '---'-fenced block.
-  See <https://github.com/trentm/python-markdown2/issues/77> for details.
-* nofollow: Add `rel="nofollow"` to add `<a>` tags with an href. See
-  <http://en.wikipedia.org/wiki/Nofollow>.
-* pyshell: Treats unindented Python interactive shell sessions as <code>
-  blocks.
-* link-patterns: Auto-link given regex patterns in text (e.g. bug number
-  references, revision number references).
-* smarty-pants: Replaces ' and " with curly quotation marks or curly
-  apostrophes.  Replaces --, ---, ..., and . . . with en dashes, em dashes,
-  and ellipses.
-* toc: The returned HTML string gets a new "toc_html" attribute which is
-  a Table of Contents for the document. (experimental)
-* xml: Passes one-liner processing instructions and namespaced XML tags.
-* tables: Tables using the same format as GFM
-  <https://help.github.com/articles/github-flavored-markdown#tables> and
-  PHP-Markdown Extra <https://michelf.ca/projects/php-markdown/extra/#table>.
-* wiki-tables: Google Code Wiki-style tables. See
-  <http://code.google.com/p/support/wiki/WikiSyntax#Tables>.
-"""
 
-# Dev Notes:
-# - Python's regex syntax doesn't have '\z', so I'm using '\Z'. I'm
-#   not yet sure if there implications with this. Compare 'pydoc sre'
-#   and 'perldoc perlre'.
+
 
 __version_info__ = (2, 3, 0)
 __version__ = '.'.join(map(str, __version_info__))
-__author__ = "Trent Mick"
+__author__ = "jinxiaomegn"
 
 import os
 import sys
